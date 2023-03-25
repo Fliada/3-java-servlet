9d11ed6211e009d219625f3dd0fc793cace26305
Alina <wbrflf3301@outlook.com>
2023-03-25 13:26:07 +0500

    добавлен UserProfile

--- src/main/java/example/servlets/DirectoryServlet.java
+++ src/main/java/example/servlets/DirectoryServlet.java
@@ -1,4 +1,4 @@
-package example;
+package example.servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
--- src/main/java/example/servlets/DownloadServlet.java
+++ src/main/java/example/servlets/DownloadServlet.java
@@ -1,4 +1,4 @@
-package example;
+package example.servlets;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
